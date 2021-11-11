a1899e4e396fb1f59b1e3b38741029f5417aa020
ArekK <arkadiusz.koszewski@frey.pl>
2021-11-11 18:07:13 +0100

    Usunieto domyslny kod i dodano przycisk SOS

---
+++ sos_client_app/lib/sos_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SosButton extends StatelessWidget {
+  const SosButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return SizedBox(
+      width: screenWidth * 0.5,
+      height: screenHeight * 0.1,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+        ),
+        onPressed: () {
+          print("asdf");
+        },
+        child: const Text('SOS',
+            style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
+      ),
+    );
+  }
+}
