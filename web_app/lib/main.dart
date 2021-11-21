702ed2137da33d74f8b01828dd68cd7afd0f1dcb
ArekK <arkadiusz.koszewski@frey.pl>
2021-11-21 16:50:07 +0100

    Apka webowa wyswietla wstepnie mape

---
+++ web_app/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+            child: FlutterMap(
+          options: MapOptions(
+            center: LatLng(52.5, 21),
+            //center: null,
+            zoom: 13.0,
+          ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c'],
+              attributionBuilder: (_) {
+                return Text("© OpenStreetMap contributors");
+              },
+            ),
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: LatLng(52.5, 21),
+                  builder: (ctx) => Container(
+                    child: FlutterLogo(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
