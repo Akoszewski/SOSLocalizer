657ca40abab828cf3b64bdaf2fe1b464e3345713
ArekK <arkadiusz.koszewski@frey.pl>
2021-12-19 15:56:04 +0100

    Usunieto # z adresu url strony

--- sos_client_app/lib/main.dart
+++ sos_client_app/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:sos_client_app/location_sender.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'main_map.dart';
 import "sos_button.dart";
 
-void main() => runApp(MyApp());
+void main() {
+  setPathUrlStrategy(); // Usuwa # w adresie url strony
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   late final LocationSender locationSender;
@@ -24,18 +28,16 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.yellow[100],
         body: Center(
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-              StreamBuilder(
-                stream: locationSender.channel.stream,
-                builder: (context, snapshot) {
-                  return MainMap(snapshot.hasData ? '${snapshot.data}' : '');
-                  // return Text(snapshot.hasData ? '${snapshot.data}' : '');
-                },
-              ),
-              SosButton(callback: _onSOS),
-            ])),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          StreamBuilder(
+            stream: locationSender.channel.stream,
+            builder: (context, snapshot) {
+              return MainMap(snapshot.hasData ? '${snapshot.data}' : '');
+            },
+          ),
+          SosButton(callback: _onSOS),
+        ])),
       ),
     );
   }
