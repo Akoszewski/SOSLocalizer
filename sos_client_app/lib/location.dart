9034061b53ba0b607ae2d4fd8ea217227d5675a7
ArekK <arkadiusz.koszewski@frey.pl>
2022-01-06 21:29:04 +0100

    Usuniecie kodu w js

--- sos_client_app/lib/location.dart
+++ sos_client_app/lib/location.dart
@@ -1,12 +1,6 @@
 import 'dart:async';
-import 'dart:html';
-import 'dart:js';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:latlong2/latlong.dart';
-
 import 'package:geolocator/geolocator.dart';
-import 'loc.dart';
-import 'package:latlong2/latlong.dart';
 
 class Location {
   Future<LatLng?> determineLocation() async {
@@ -32,27 +26,7 @@ class Location {
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
-    if (kIsWeb) {
-      Completer completer = Completer();
-      getCurrentPosition(allowInterop((pos) {
-        completer.complete(pos);
-      }));
-      Geoposition pos = await completer.future;
-      if (pos.coords != null) {
-        if (pos.coords!.latitude != null && pos.coords!.longitude != null) {
-          num lat = pos.coords!.latitude!;
-          num long = pos.coords!.longitude!;
-          coords = LatLng(lat.toDouble(), long.toDouble());
-        } else {
-          coords = null; // Moze zwrocic future error
-        }
-      } else {
-        coords = null;
-      }
-      return coords;
-    } else {
-      Position pos = await Geolocator.getCurrentPosition();
-      return LatLng(pos.latitude, pos.longitude);
-    }
+    Position pos = await Geolocator.getCurrentPosition();
+    return LatLng(pos.latitude, pos.longitude);
   }
 }
