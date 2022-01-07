aa08c397f965ada8ddf10baa8ace952221d9710c
ArekK <arkadiusz.koszewski@frey.pl>
2022-01-07 17:54:57 +0100

    Dodatkowe komentarze w skryptach, serwer generuje id objektow

--- server_app/docker/backend/server.js
+++ server_app/docker/backend/server.js
@@ -2,10 +2,20 @@ var WebSocketServer = require("ws").Server;
 
 var wss = new WebSocketServer({port:7777});
 
+var last_id = 0;
+
+function generateId() {
+    return last_id++;
+}
+
 wss.on('connection', function connection(ws) {
     ws.on('message', function(message) {
-        wss.broadcast(message.toString());
-    })
+        var msg = JSON.parse(message);
+        if (msg.command == "SOS") {
+            msg.id = generateId();
+        }
+        wss.broadcast(JSON.stringify(msg));
+    });
 });
 
 wss.broadcast = function broadcast(msg) {
--- server_app/scripts/upload.sh
+++ server_app/scripts/upload.sh
@@ -1,11 +1,13 @@
-# w folderze z apka: flutter build web --no-sound-null-safety (geolokator sobie nie radzi z nullsafety)
-# scp -i ../keys/private.pem ../server.js ubuntu@3.68.186.250:~/servers/sosapp/
+# w folderze z apka: flutter build web
+
+# wrzucanie skompilowanego kodu
 scp -i ../keys/private.pem -r ../../sos_client_app/build/web/* ubuntu@3.68.186.250:~/docker/test/pliki/
-# ssh -i ../keys/private.pem ubuntu@3.68.186.250 'cp -r ~/web/* ~/docker/test/pliki/; rm -r ~/web/;'
 
+# wrzucanie plikow docker-compose (jesli byly edytowane)
 scp -i ../keys/private.pem -r ../docker/docker-compose-test.yml ubuntu@3.68.186.250:~/docker/test/docker-compose.yml
-
 scp -i ../keys/private.pem -r ../docker/docker-compose-proxy.yml ubuntu@3.68.186.250:~/docker/nginx/docker-compose.yml
+# + trzeba uruchomic kontenery "docker-compose up -d" albo "docker-compose restart" w odpowiednich folderach
 
-
+# wrzucanie backendu
 scp -i ../keys/private.pem ../docker/backend/* ubuntu@3.68.186.250:~/docker/backend/
+# + na serwerze "docker build -t backend ."; "docker-compose up -d"
